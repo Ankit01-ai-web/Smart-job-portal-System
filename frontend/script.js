e2ecaf067b083a8a7979c77642baa2ce2b7269e5
Ankit Negi <ankitnegi669@gmail.com>
2026-03-19 16:22:15 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -6,7 +6,7 @@ async function register() {
     role: "user"
   };
 
-  const res = await fetch("http://localhost:5000/register", {
+  const res = await fetch("https://smart-job-portal-system-eftt.onrender.com/register", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data)
