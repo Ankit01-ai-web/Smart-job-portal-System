7c6e439e1a38d9af8f5f2c62a3fb439bd9a283f8
Ankit Negi <ankitnegi669@gmail.com>
2026-03-19 15:59:05 +0530

    fixed mysql require

--- backend-node/server.js
+++ backend-node/server.js
@@ -1,6 +1,6 @@
 const cors = require("cors");
 const express = require("express");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 
